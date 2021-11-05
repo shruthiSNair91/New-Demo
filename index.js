7a8e52b919e8db9b183a3841cad0645af124e9c9
shruthiSNair91 <shruthi.snair91@gmail.com>
2021-11-05 16:43:15 +0530

    changes

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var x = document.getElementById("head1");
+x.innerText= "you are hacked..";
+//x.innerHTML= "<a href='#'> Download</a>";
+x.style.color="grey";
+var y= document.getElementsByClassName("para1");
+y[0].innerHTML="<h4>i am changed</h4>";
+//y[0].innerText="april";
+y[0].style.padding= "10px";
+y[0].style.color="yellow";
+//y[0].setAttribute("hidden","false");
+
